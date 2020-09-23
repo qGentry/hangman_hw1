74b2c7bd353869a7395cc72846c93a73354c347f
Fisin Filipp Gennadevich <f.fisin@tinkoff.ru>
2020-09-23 21:16:39 +0300

    Some fixes for code coverage

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import setup, find_packages
 
 
 def main():
-
     console_scripts = [
         "run-hangman = bin.run_hangman:main",
     ]
 
     setup(
         name="games",
-        version="1.1",
+        version="1.0",
         author="Fisin Philipp",
         package_dir={"": "src"},
+        license="MIT",
+        url="https://github.com/qGentry/hangman_hw1",
         packages=find_packages("src"),
         description="Simple console games",
         entry_points={
             "console_scripts": console_scripts
-        }
+        },
     )
 
 
